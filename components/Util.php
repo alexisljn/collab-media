bd981e45ebdfb24aec5412fa018e5bd8730bc17f
Emile Calixte <calixte.emile@gmail.com>
2020-01-31 22:02:05 +0100

    Create Util Class And Roles Constants
    Created Util class (it's a toolbox)
    - Created roles constants in Util class

---
+++ components/Util.php
@@ -0,0 +1,13 @@
+<?php
+
+
+namespace app\components;
+
+
+class Util
+{
+    public const USER_ROLE_MEMBER = 'member';
+    public const USER_ROLE_REVIEWER = 'reviewer';
+    public const USER_ROLE_PUBLISHER = 'publisher';
+    public const USER_ROLE_ADMIN = 'admin';
+}

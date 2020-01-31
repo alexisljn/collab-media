6be1d22d9cfc8ac7c8dff082fb2a7967ab71a963
SKLadeR <guenole.cherubin@gmail.com>
2020-01-31 10:42:42 +0100

    Added Bootstrap And JQuery

--- views/layouts/main.php
+++ views/layouts/main.php
@@ -19,6 +19,9 @@ AppAsset::register($this);
     <meta charset="<?= Yii::$app->charset ?>">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="/css/bootstrap.min.css">
+    <script src="/js/bootstrap.min.js"></script>
+    <script src="/js/jquery-3.4.1.min.js"></script>
     <?php $this->registerCsrfMetaTags() ?>
     <title><?= Html::encode($this->title) ?></title>
     <?php $this->head() ?>

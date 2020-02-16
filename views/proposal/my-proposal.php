04271360ff5df9e2288fda688c512968bd1bcee5
Alexis <alexis.lejeune@gmx.fr>
2020-02-16 09:10:00 +0100

    Display Of Proposal Informations In View
    Title, creation date, last edit date, status, social media and content.

--- views/proposal/my-proposal.php
+++ views/proposal/my-proposal.php
@@ -2,7 +2,20 @@
 /** @var \app\models\databaseModels\Proposal $selectedProposal */
 /** @var \app\models\databaseModels\ProposalContentHistory $lastProposalContent */
 /** @var \app\models\databaseModels\Review|\app\models\databaseModels\Comment|\app\models\databaseModels\ProposalContentHistory $chronologicalStream */
-
-//echo'toto';
 ?>
-<h1><?= $selectedProposal ?></h1>
+
+<!-- Proposal informations -->
+
+<h1><?= $selectedProposal->title ?></h1>
+<p>Created at <?= $selectedProposal->date ?></p>
+<?php if($selectedProposal->date != $lastProposalContent->date) { ?>
+    <p>Last edit : <?= $lastProposalContent->date ?></p>
+<?php } ?>
+<p>Status : <?= $selectedProposal->status ?></p>
+<?php if (!is_null($selectedProposal->social_media)) { ?>
+    <p>Published on : <?= $selectedProposal->social ?></p>
+<?php } ?>
+<p><?= $lastProposalContent->content ?></p>
+
+<!-- Chronological Stream -->
+

6e57ece84ae8cfffc2f55330924e770b55e5ce0d
Alexis <alexis.lejeune@gmx.fr>
2020-02-16 15:08:13 +0100

    Removal Of Web Config Comment
    In order to keep this file clean.

--- config/web.php
+++ config/web.php
@@ -63,7 +63,6 @@ $config = [
             'enablePrettyUrl' => true,
             'showScriptName' => false,
             'rules' => [
-               // '<controller:\w+>/<id:\d+>' => '<controller>/view',
                 'proposal/my-proposals/<id:\d+>' => 'proposal/my-proposals'
             ],
         ],

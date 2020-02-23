cc7f08cc1f9a83190187341a4baf9aa8f928a281
Alexis <alexis.lejeune@gmx.fr>
2020-02-23 16:28:36 +0100

    Get HTML Input From Markdown Editor
    After a click of the user

--- views/proposal/create-proposal.php
+++ views/proposal/create-proposal.php
@@ -4,13 +4,18 @@ echo 'CREATE PROPOSAL';
 
 MARKDOWN EDITOR
 <div id="Proposalcontent" class="editSection"></div>
+
+<button id="toto">Click</button>
 <script type="text/javascript">
    let editor = new tui.Editor({
        el: document.querySelector('.editSection'),
        previewStyle: 'vertical',
        height: '300px',
        initialEditType: 'markdown'
-   })
+   });
+    let button = document.querySelector('#toto').addEventListener('click', ()=> {
+        console.log(editor.getHtml());
+    });
 </script>
 
 

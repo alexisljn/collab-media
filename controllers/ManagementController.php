22b89e79a181467fd9868dae036bf9ca3327551f
SKLadeR <guenole.cherubin@gmail.com>
2020-02-24 18:53:47 +0100

    Had The PHP Doc In Management Controller
    Haded the PHP doc in the management controller

--- controllers/ManagementController.php
+++ controllers/ManagementController.php
@@ -35,15 +35,28 @@ class ManagementController extends MainController
 
     }
 
+    /**
+     * Display a page where the account of the user selected in actionAccounts
+     * $id is an id from a User
+     */
     private function actionModifiyAccount($id)
     {
         $user =  User::findOne(['id' => $id]);
-        //dd($user);
+
         $form = new ModifyAccountForm();
+
+        /**
+         * this if populate $form with the datas from $_POST
+         * and check if it respect the model from ModifyAccountForm
+         * Then calls to function aupdateAccount
+         */
         if($form->load($_POST) && $form->validate()) {
             $this->updateAccount($form, $user);
         }
 
+        /**
+         * Affect to the form the value we have in the user object
+         */
         $form->firstname    = $user->firstname;
         $form->lastname     = $user->lastname;
         $form->email        = $user->email;
@@ -51,7 +64,9 @@ class ManagementController extends MainController
         $form->is_active    = $user->is_active;
 
 
-
+        /**
+         * Return the form so that we can use it in the view modifyAccount.php
+         */
         return $this->render('modifyAccount', [
             'model'=>$form,
         ]);
@@ -59,16 +74,22 @@ class ManagementController extends MainController
 
     private function updateAccount(ModifyAccountForm $form, $user)
     {
-
+        /**
+         * Populate the user object with the datas from the form
+         */
         $user->firstname    = $form->firstname;
         $user->lastname     = $form->lastname;
         $user->email        = $form->email;
         $user->is_validated = $form->is_validated;
         $user->is_active    = $form->is_active;
 
-
+        /**
+         * Try to save it in the database
+         * If the condition equals true, then the normal page will be displayed witht the changes done
+         * If the condition equals false, then we echo the strnig save failed
+         */
         if($user->save()){
-           
+
 
         } else {
             echo "save failed.";
@@ -76,3 +97,4 @@ class ManagementController extends MainController
 
     }
 }
+?>

71dffb70839f76b7bccde07be07a62532e5f48ea
Emile Calixte <calixte.emile@gmail.com>
2020-02-26 12:11:43 +0100

    Moved LoginForm

--- controllers/SiteController.php
+++ controllers/SiteController.php
@@ -7,7 +7,7 @@ use Yii;
 use yii\filters\AccessControl;
 use yii\web\Response;
 use yii\filters\VerbFilter;
-use app\models\LoginForm;
+use app\models\forms\LoginForm;
 
 class SiteController extends MainController
 {

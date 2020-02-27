f8896b456c05174e44a2b18b0d0326a2bce5226f
alexisljn <alexis.lejeune@gmx.fr>
2020-02-27 12:38:46 +0100

    Creation of File Content History Entity
    For chronological Stream

---
+++ models/databaseModels/FileContentHistory.php
@@ -0,0 +1,63 @@
+<?php
+
+namespace app\models\databaseModels;
+
+use Yii;
+
+/**
+ * This is the model class for table "file_content_history".
+ *
+ * @property int $id
+ * @property int $proposal_id
+ * @property string $date
+ * @property string $path
+ *
+ * @property Proposal $proposal
+ */
+class FileContentHistory extends \yii\db\ActiveRecord
+{
+    /**
+     * {@inheritdoc}
+     */
+    public static function tableName()
+    {
+        return 'file_content_history';
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function rules()
+    {
+        return [
+            [['proposal_id', 'date', 'path'], 'required'],
+            [['proposal_id'], 'integer'],
+            [['date'], 'safe'],
+            [['path'], 'string', 'max' => 255],
+            [['proposal_id'], 'exist', 'skipOnError' => true, 'targetClass' => Proposal::className(), 'targetAttribute' => ['proposal_id' => 'id']],
+        ];
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function attributeLabels()
+    {
+        return [
+            'id' => 'ID',
+            'proposal_id' => 'Proposal ID',
+            'date' => 'Date',
+            'path' => 'Path',
+        ];
+    }
+
+    /**
+     * Gets query for [[Proposal]].
+     *
+     * @return \yii\db\ActiveQuery
+     */
+    public function getProposal()
+    {
+        return $this->hasOne(Proposal::className(), ['id' => 'proposal_id']);
+    }
+}

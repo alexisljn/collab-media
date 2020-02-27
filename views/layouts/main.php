1cf2f605ba6dea16fcfde4e99e3cc8eb96601560
Emile Calixte <calixte.emile@gmail.com>
2020-02-27 12:16:11 +0100

    Remove Unused imports in main layout
    Removed import of Yii navbar and breadcrumbs, we don't use them

--- views/layouts/main.php
+++ views/layouts/main.php
@@ -6,9 +6,6 @@
 use app\controllers\mainController\MainController;
 use app\widgets\Alert;
 use yii\helpers\Html;
-use yii\bootstrap\Nav;
-use yii\bootstrap\NavBar;
-use yii\widgets\Breadcrumbs;
 use app\assets\AppAsset;
 
 AppAsset::register($this);

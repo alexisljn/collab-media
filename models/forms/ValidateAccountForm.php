7e54d42a630ae98fd5b20beedb30cfe23342edda
Guenole <guenole.cherubin@gmail.com>
2020-03-16 12:07:44 +0100

    Create Password For New Account
    Added the views and the actions to create a password.
    Added a form for the validate-account view.
    Login the user if the save succeded.

---
+++ models/forms/ValidateAccountForm.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace app\models\forms;
+
+use yii\base\Model;
+class ValidateAccountForm extends Model
+{
+    public $password;
+    public $confirmPassword;
+
+    public function rules()
+    {
+        return [
+            // username and password are both required
+            [['password', 'confirmPassword'], 'required'],
+            // password is validated by validatePassword()
+            ['password', 'validatePassword'],
+            // password and confirmPassword must match
+            ['confirmPassword', 'compare', 'compareAttribute' => 'password', 'message' => 'Password and password confirmation fields don\'t match.'],
+        ];
+    }
+
+    public function validatePassword($attribute)
+    {
+        if(mb_strlen($this->password) < 8) {
+            $this->addError($attribute, 'Password must be at least 8 characters long.');
+        }
+    }
+}

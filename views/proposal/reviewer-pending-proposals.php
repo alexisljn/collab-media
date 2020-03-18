20b0c58fc51d5b7cce2185172d53c622ab4e15dc
Alexis <alexis.lejeune@gmx.fr>
2020-03-18 19:56:09 +0100

    Start reviewer proposals style
    ActiveDataProvider style is needed

--- views/proposal/reviewer-pending-proposals.php
+++ views/proposal/reviewer-pending-proposals.php
@@ -1,122 +1,158 @@
 <?php
-/** @var \yii\data\ActiveDataProvider $noReviewedProposalsByAReviewerDataProvider */
-/** @var \yii\data\ActiveDataProvider $reviewedProposalsByAReviewerDataProvider */
-
-
+/** @var \yii\data\ActiveDataProvider $proposalsToReviewActiveDataProvider */
+/** @var \yii\data\ActiveDataProvider $reviewedProposalsActiveDataProvider */
 ?>
-<strong>No reviewed proposals for a reviewer</strong>
-<?php \yii\widgets\Pjax::begin();
-echo \yii\grid\GridView::widget([
-    'dataProvider' => $noReviewedProposalsByAReviewerDataProvider,
-    'columns' => [
-        [
-            'attribute' => 'title',
-            'label' => 'Title',
-            'format' => 'raw',
-            'value' => function($proposal)
-            {
+<div class="row">
+    <h1 class="full-border">Pending proposals</h1>
+</div>
+<div class="row">
+    <div class="col-lg-12">
+        <ul class="nav nav-pills mb-3" id="dashboard-tabs" role="tablist">
+            <li class="nav-item">
+                <a class="nav-link active"
+                   id="proposals-to-review-tab"
+                   data-toggle="tab"
+                   href="#proposals-to-review"
+                   role="tab"
+                   aria-controls="proposals-to-review"
+                   aria-selected="true">Proposals to review</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link"
+                   id="reviewed-proposals-tab"
+                   data-toggle="tab"
+                   href="#reviewed-proposals"
+                   role="tab"
+                   aria-controls="reviewed-proposals"
+                   aria-selected="false">Reviewed proposals</a>
+            </li>
+        </ul>
+    </div>
+</div>
+<div class="row">
+    <div id="proposals-tabs-content" class="col-lg-12 tab-content">
+        <div id="proposals-to-review"
+             class="tab-pane fade show active"
+             role="tabpanel"
+             aria-labelledby="proposals-to-review-tab">
+            <?php \yii\widgets\Pjax::begin();
+            echo \yii\grid\GridView::widget([
+                'dataProvider' => $proposalsToReviewActiveDataProvider,
+                'columns' => [
+                    [
+                        'attribute' => 'title',
+                        'label' => 'Title',
+                        'format' => 'raw',
+                        'value' => function($proposal)
+                        {
 
-                /** @var \app\models\databaseModels\Proposal $proposal */
+                            /** @var \app\models\databaseModels\Proposal $proposal */
 
-                return '<a data-pjax="0" href="/proposal/proposal/'. $proposal->id . '">' . \yii\helpers\Html::encode($proposal->title) . '</a>';
-            }
-        ],
-        [
-            'attribute' => 'date',
-            'label' => 'Date'
-        ],
-        [
-            'attribute' => 'has_review',
-            'label' => 'Reviewed ?',
-            'value' => function($proposal) {
-                /** @var \app\models\databaseModels\Proposal $proposal */
-                if ($proposal->getReviews()->count()) {
-                    return 'Yes';
-                }
-                return 'No';
-            }
-        ],
-        [
-            'label' => 'Approval',
-            'value' => function($proposal)
-            {
-                /** @var \app\models\databaseModels\Proposal $proposal */
+                            return '<a data-pjax="0" href="/proposal/proposal/'. $proposal->id . '">' . \yii\helpers\Html::encode($proposal->title) . '</a>';
+                        }
+                    ],
+                    [
+                        'attribute' => 'date',
+                        'label' => 'Date'
+                    ],
+                    [
+                        'attribute' => 'has_review',
+                        'label' => 'Reviewed ?',
+                        'value' => function($proposal) {
+                            /** @var \app\models\databaseModels\Proposal $proposal */
+                            if ($proposal->getReviews()->count()) {
+                                return 'Yes';
+                            }
+                            return 'No';
+                        }
+                    ],
+                    [
+                        'label' => 'Approval',
+                        'value' => function($proposal)
+                        {
+                            /** @var \app\models\databaseModels\Proposal $proposal */
 
-                return $proposal->getReviews()->where(['status' => 'approved'])->count();
-            }
-        ],
-        [
-            'label' => 'Disapproval',
-            'value' => function($proposal)
-            {
-                /** @var \app\models\databaseModels\Proposal $proposal */
+                            return $proposal->getReviews()->where(['status' => 'approved'])->count();
+                        }
+                    ],
+                    [
+                        'label' => 'Disapproval',
+                        'value' => function($proposal)
+                        {
+                            /** @var \app\models\databaseModels\Proposal $proposal */
 
-                return $proposal->getReviews()->where(['status' => 'disapproved'])->count();
-            }
-        ],
-        [
-            'label' => 'Comments',
-            'value' => function($proposal)
-            {
-                /** @var \app\models\databaseModels\Proposal $proposal */
+                            return $proposal->getReviews()->where(['status' => 'disapproved'])->count();
+                        }
+                    ],
+                    [
+                        'label' => 'Comments',
+                        'value' => function($proposal)
+                        {
+                            /** @var \app\models\databaseModels\Proposal $proposal */
 
-                return $proposal->getComments()->count();
-            }
-        ]
+                            return $proposal->getComments()->count();
+                        }
+                    ]
 
-    ]
-]) ;
-\yii\widgets\Pjax::end();?>
-<strong>reviewed proposals for a reviewer</strong>
-<?php
-\yii\widgets\Pjax::begin();
-echo \yii\grid\GridView::widget([
-    'dataProvider' => $reviewedProposalsByAReviewerDataProvider,
-    'columns' => [
-        [
-            'attribute' => 'title',
-            'label' => 'Title',
-            'format' => 'raw',
-            'value' => function($proposal)
-            {
+                ]
+            ]) ;
+            \yii\widgets\Pjax::end();?>
+        </div>
+        <div id="reviewed-proposals"
+             class="tab-pane fade"
+             role="tabpanel"
+             aria-labelledby="reviewed-proposals-tab">
+            <?php
+            \yii\widgets\Pjax::begin();
+            echo \yii\grid\GridView::widget([
+                'dataProvider' => $reviewedProposalsActiveDataProvider,
+                'columns' => [
+                    [
+                        'attribute' => 'title',
+                        'label' => 'Title',
+                        'format' => 'raw',
+                        'value' => function($proposal)
+                        {
 
-                /** @var \app\models\databaseModels\Proposal $proposal */
+                            /** @var \app\models\databaseModels\Proposal $proposal */
 
-                return '<a data-pjax="0" href="/proposal/proposal/'. $proposal->id . '">' . \yii\helpers\Html::encode($proposal->title) . '</a>';
-            }
-        ],
-        [
-            'attribute' => 'date',
-            'label' => 'Date'
-        ],
-        [
-            'label' => 'Approval',
-            'value' => function($proposal)
-            {
-                /** @var \app\models\databaseModels\Proposal $proposal */
+                            return '<a data-pjax="0" href="/proposal/proposal/'. $proposal->id . '">' . \yii\helpers\Html::encode($proposal->title) . '</a>';
+                        }
+                    ],
+                    [
+                        'attribute' => 'date',
+                        'label' => 'Date'
+                    ],
+                    [
+                        'label' => 'Approval',
+                        'value' => function($proposal)
+                        {
+                            /** @var \app\models\databaseModels\Proposal $proposal */
 
-                return $proposal->getReviews()->where(['status' => 'approved'])->count();
-            }
-        ],
-        [
-            'label' => 'Disapproval',
-            'value' => function($proposal)
-            {
-                /** @var \app\models\databaseModels\Proposal $proposal */
+                            return $proposal->getReviews()->where(['status' => 'approved'])->count();
+                        }
+                    ],
+                    [
+                        'label' => 'Disapproval',
+                        'value' => function($proposal)
+                        {
+                            /** @var \app\models\databaseModels\Proposal $proposal */
 
-                return $proposal->getReviews()->where(['status' => 'disapproved'])->count();
-            }
-        ],
-        [
-            'label' => 'Comments',
-            'value' => function($proposal)
-            {
-                /** @var \app\models\databaseModels\Proposal $proposal */
+                            return $proposal->getReviews()->where(['status' => 'disapproved'])->count();
+                        }
+                    ],
+                    [
+                        'label' => 'Comments',
+                        'value' => function($proposal)
+                        {
+                            /** @var \app\models\databaseModels\Proposal $proposal */
 
-                return $proposal->getComments()->count();
-            }
-        ]
+                            return $proposal->getComments()->count();
+                        }
+                    ]
 
-    ]
-]);
-\yii\widgets\Pjax::end();?>
+                ]
+            ]);
+            \yii\widgets\Pjax::end();?>
+    </div>
+</div>

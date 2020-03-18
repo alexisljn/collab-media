6e46ce264d8f5c15916a5be9a0c394804050d5eb
Emile Calixte <calixte.emile@gmail.com>
2020-03-19 00:57:28 +0100

    Re-order admin account view

--- views/management/modify-account.php
+++ views/management/modify-account.php
@@ -11,59 +11,12 @@ use yii\bootstrap\ActiveForm;
 
 $this->title = 'Modify Account';
 ?>
-    <h1><?= Html::encode($this->title) ?></h1>
+<div class="row mb-5">
+    <div class="col-12">
+        <h1><?= Html::encode($this->title) ?></h1>
 
-    <?php $formModifyAccount = ActiveForm::begin([
-        'id' => 'modify-account-form',
-        'method' => 'post',
-        'layout' => 'horizontal',
-        'fieldConfig' => [
-            'template' => "{label}\n<div class=\"col-lg-3\">{input}</div>\n<div class=\"col-lg-8\">{error}</div>",
-            'labelOptions' => ['class' => 'col-lg-1 control-label'],
-        ],
-    ]);?>
-
-    <?= $formModifyAccount->field($formModifyAccountModel, 'firstname')?>
-
-    <?= $formModifyAccount->field($formModifyAccountModel, 'lastname')?>
-
-    <?= $formModifyAccount->field($formModifyAccountModel, 'email')?>
-
-    <?= $formModifyAccount->field($formModifyAccountModel, 'role')->dropDownList([
-            User::USER_ROLE_MEMBER => 'Member',
-            User::USER_ROLE_REVIEWER => 'Reviewer',
-            User::USER_ROLE_PUBLISHER => 'Publisher',
-            User::USER_ROLE_ADMIN => 'Admin'
-    ],[
-        'options'=>[
-            'role' => ['selected' => true]
-        ]]) ?>
-
-    <?= $formModifyAccount->field($formModifyAccountModel,'is_active')->checkbox(); ?>
-
-    <div class="form-group">
-        <div class="col-lg-offset-1 col-lg-11">
-            <?= Html::submitButton('Modify Account', [
-                    'class' => 'btn btn-primary', 'name' => 'modify-account-button'
-            ]) ?>
-        </div>
-    </div>
-
-    <?php ActiveForm::end();?>
-
-    <h1>Reset User Password</h1>
-    <p>You can reset the user password by clicking on the button below. He will receive an email to define a new password.</p>
-    <form method="post" action="/management/reset-password/<?= $user->id; ?>">
-        <?= Html::hiddenInput(Yii::$app->request->csrfParam, Yii::$app->request->csrfToken) ?>
-        <input hidden name="redirect" value="/management/accounts/<?= $user->id; ?>">
-        <button type="submit">Reset Password</button>
-    </form>
-
-
-    <?php
-     if (in_array($formModifyAccountModel->role, [User::USER_ROLE_PUBLISHER, User::USER_ROLE_ADMIN])) {
-        $formModifySocialMediaPermission = ActiveForm::begin([
-            'id' => 'modify-social-media-form',
+        <?php $formModifyAccount = ActiveForm::begin([
+            'id' => 'modify-account-form',
             'method' => 'post',
             'layout' => 'horizontal',
             'fieldConfig' => [
@@ -71,18 +24,79 @@ $this->title = 'Modify Account';
                 'labelOptions' => ['class' => 'col-lg-1 control-label'],
             ],
         ]);?>
-        <?= $formModifySocialMediaPermission->field($formSocialMediaPermissionModel,'facebook_enabled')->checkbox(); ?>
-        <?= $formModifySocialMediaPermission->field($formSocialMediaPermissionModel,'twitter_enabled')->checkbox(); ?>
-        <?= $formModifySocialMediaPermission->field($formSocialMediaPermissionModel,'linkedin_enabled')->checkbox(); ?>
+
+        <?= $formModifyAccount->field($formModifyAccountModel, 'firstname')?>
+
+        <?= $formModifyAccount->field($formModifyAccountModel, 'lastname')?>
+
+        <?= $formModifyAccount->field($formModifyAccountModel, 'email')?>
+
+        <?= $formModifyAccount->field($formModifyAccountModel, 'role')->dropDownList([
+            User::USER_ROLE_MEMBER => 'Member',
+            User::USER_ROLE_REVIEWER => 'Reviewer',
+            User::USER_ROLE_PUBLISHER => 'Publisher',
+            User::USER_ROLE_ADMIN => 'Admin'
+        ],[
+            'options'=>[
+                'role' => ['selected' => true]
+            ]]) ?>
+
+        <?= $formModifyAccount->field($formModifyAccountModel,'is_active')->checkbox(); ?>
+
         <div class="form-group">
             <div class="col-lg-offset-1 col-lg-11">
-                <?= Html::submitButton('Modify Permission', [
-                        'class' => 'btn btn-primary', 'name' => 'modify-social-media-permission-button'
+                <?= Html::submitButton('Modify Account', [
+                    'class' => 'btn btn-primary', 'name' => 'modify-account-button'
                 ]) ?>
             </div>
         </div>
-    <?php ActiveForm::end();
-    } ?>
+
+        <?php ActiveForm::end();?>
+    </div>
+</div>
 
 
+<?php
+if (in_array($formModifyAccountModel->role, [User::USER_ROLE_PUBLISHER, User::USER_ROLE_ADMIN])) {
+    ?>
+    <div class="row mb-5">
+        <div class="col-12">
+            <h2 class="h5">Publisher permissions</h2>
+            <p>Choose on which social network this user can publish content</p>
+            <?php
+            $formModifySocialMediaPermission = ActiveForm::begin([
+                'id' => 'modify-social-media-form',
+                'method' => 'post',
+                'layout' => 'horizontal',
+                'fieldConfig' => [
+                    'template' => "{label}\n<div class=\"col-lg-3\">{input}</div>\n<div class=\"col-lg-8\">{error}</div>",
+                    'labelOptions' => ['class' => 'col-lg-1 control-label'],
+                ],
+            ]);?>
+            <?= $formModifySocialMediaPermission->field($formSocialMediaPermissionModel,'facebook_enabled')->checkbox()->label('Facebook'); ?>
+            <?= $formModifySocialMediaPermission->field($formSocialMediaPermissionModel,'twitter_enabled')->checkbox()->label('Twitter'); ?>
+            <?= $formModifySocialMediaPermission->field($formSocialMediaPermissionModel,'linkedin_enabled')->checkbox()->label('Linkedin'); ?>
+            <div class="form-group">
+                <div class="col-lg-offset-1 col-lg-11">
+                    <?= Html::submitButton('Modify Permissions', [
+                        'class' => 'btn btn-primary', 'name' => 'modify-social-media-permission-button'
+                    ]) ?>
+                </div>
+            </div>
+        </div>
+    </div>
+<?php ActiveForm::end();
+} ?>
 
+
+<div class="row mb-5">
+    <div class="col-12">
+        <h2 class="h5">Reset User Password</h2>
+        <p>You can reset the user password by clicking on the button below. He will receive an email to define a new password.</p>
+        <form method="post" action="/management/reset-password/<?= $user->id; ?>">
+            <?= Html::hiddenInput(Yii::$app->request->csrfParam, Yii::$app->request->csrfToken) ?>
+            <input hidden name="redirect" value="/management/accounts/<?= $user->id; ?>">
+            <button type="submit" class="btn btn-not-outline">Reset Password</button>
+        </form>
+    </div>
+</div>

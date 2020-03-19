f27a3eecae921bca803a56a03b6caea8eae2eec1
Alexis <alexis.lejeune@gmx.fr>
2020-03-19 13:49:37 +0100

    Correct sort of reviewed proposals

--- controllers/ProposalController.php
+++ controllers/ProposalController.php
@@ -349,6 +349,7 @@ class ProposalController extends MainController
                     ->select('proposal_id')
                     ->from('review')
                     ->where(['reviewer_id' => self::getCurrentUser()->id])
+                    ->andWhere(['not', ['status' => \app\models\Review::REVIEW_STATUS_CANCELLED]])
                     ->column()
             ])
             ->andWhere(['status' => 'pending']),
